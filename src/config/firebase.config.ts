3b9caa297211bfa598bb8f88d1507a0263c09f96
luskoeqk <77803201+luskoeqk@users.noreply.github.com>
2022-09-24 17:09:13 +0300

    Update firebase.config.ts

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,7 +1,7 @@
 
 export const firebaseConfig = {
     firebaseConfig: {
-        apiKey: `AIzaSyAt8JtZ_1RDu_470iVkARKAUjO_7HFoWDM`,
+        apiKey: ``,
         authDomain: "onestore-8e27a.firebaseapp.com",
         databaseURL: "https://onestore-8e27a-default-rtdb.europe-west1.firebasedatabase.app",
         projectId: "onestore-8e27a",
